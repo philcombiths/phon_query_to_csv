20f9b38a5a208066e33220664ccf37b3f0622d95
Philip Combiths <philcombiths@gmail.com>
2024-07-02 06:25:48 -0500

    renamed to main and logging

--- src/phon_query_to_csv/logging_config.py
+++ src/phon_query_to_csv/logging_config.py
@@ -3,56 +3,32 @@ Logging configuration
 """
 
 import logging
-from sys import stdout
 
-def setup_logging(loglevel, logfile='csv_compiler_errors.txt'):
+def setup_logging(loglevel,file_name, logfile='phon_query_to_csv.log'):
     """Setup basic logging for script execution
 
     Args:
       loglevel (int): minimum loglevel for emitting messages
       logfile (str): log file to save messages to
-
-    Raises:
-      ValueError: if loglevel is not a valid logging level
-      FileNotFoundError: if log file cannot be opened
     """
-    # Check if loglevel is valid
-    if not isinstance(loglevel, int) or not 0 <= loglevel <= logging.DEBUG:
-        raise ValueError("loglevel must be an integer between 0 and {}".format(logging.DEBUG))
 
-    # Set up logging format
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(
-        level=loglevel, stream=stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
-    )
+    logger = logging.getLogger(file_name)
+    logger.propagate = False # Prevents duplicate logging to console
+    logger.setLevel(loglevel)
 
-    # Get logger instance
-    log = logging.getLogger(__name__)
+    # create console handler and set    level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(loglevel)
+    ch.setFormatter(logging.Formatter('%(message)s'))
 
-    # Set up formatter
-    formatter = logging.Formatter("%(message)s - %(levelname)s - %(asctime)s")
-    formatter2 = logging.Formatter("%(message)s")
+    # create file handler and set level to debug
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(loglevel)
+    fh.setFormatter(logging.Formatter('%(message)s | %(asctime)s | %(levelname)s | %(name)s '))
 
-    # Clear existing handlers
-    if log.hasHandlers():
-        log.handlers.clear()
+    # add handlers to logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
 
-    try:
-        # Set up file handler
-        fh = logging.FileHandler(logfile)
-        fh.setLevel(logging.DEBUG)
-        fh.setFormatter(formatter2)
-
-        # Set up console handler
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        ch.setFormatter(formatter)
-
-        # Add handlers to logger
-        log.addHandler(fh)
-        log.addHandler(ch)
-    except IOError as e:
-        raise FileNotFoundError("Error opening log file: {}".format(e))
-
-    return log
+    return logger
         
