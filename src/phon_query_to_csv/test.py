f94aacb75d89d26c2eeaeaca132fb5d3a962ffc7
Francesco Vial <fzvial.design@gmail.com>
2026-03-19 17:00:34 -0500

    Reformatted code to be more modular and clean

--- src/phon_query_to_csv/test.py
+++ src/phon_query_to_csv/test.py
@@ -1,92 +1,92 @@
-import tkinter as tk
-import tkinter.font as tkfont
-import tkinter.filedialog as tkfiledialog
+from tkinter import Tk
 
-def test_page(parameters):
-    l_test = tk.Label(root, text = "Test!", font = tkfont.Font(family = "Arial", size = 16, weight = tkfont.NORMAL))
-    l_test.grid(row = 0, column = 0)
+from tkinter import Frame
+from tkinter import Label
+from tkinter import Button
+from tkinter import font
 
-    print(parameters)
+from tkinter import filedialog as dialog
 
-def next(widgets, parameters, page):
-    for widget in widgets:
-        widget.place_forget()
+def test(stage, parameters):
+    text = Label(stage, text = "Test!")
+    text.place(relx = 0.5, x = 0, y = 20, anchor = "center")
 
-    page(parameters)
+def specifications(stage, parameters):
+    prompt = Label(stage, text = "Would you like to run the default values?")
+    prompt.place(relx = 0.5, x = 0, y = 20, anchor = "n")
 
-def specifications(parameters):
-    widgets = []
+    yes = Button(stage, text = "Yes", width = 5)
+    yes.place(relx = 0.5, x = -40, y = 60, anchor = "n")
 
-    l_prompt = tk.Label(root, text = "Would you like to run the default values?", font = tkfont.Font(family = "Arial", size = 12, weight = tkfont.NORMAL))
-    l_prompt.place(relx = 0.5, x = 0, y = 200, anchor = "n")
+    yes.config(command = lambda : sketch(stage, test, parameters, None, None))
 
-    widgets.append(l_prompt)
+    no = Button(stage, text = "No", width = 5)
+    no.place(relx = 0.5, x = 40, y = 60, anchor = "n")
 
-    b_yes = tk.Button(root, text = "Yes", width = 5, height = 2, font = tkfont.Font(family = "Arial", size = 12, weight = tkfont.NORMAL), command = lambda : next(widgets, parameters, test_page))
-    b_yes.place(relx = 0.5, x = -40, y = 240, anchor = "n")
+    no.config(command = lambda : sketch(stage, test, parameters, None, None))
 
-    widgets.append(b_yes)
+def directory(stage, parameters):
+    prompt = Label(stage, text = "Please choose the directory of analysis")
+    prompt.place(relx = 0.5, x = 0, y = 20, anchor = "n")
 
-    b_no = tk.Button(root, text = "No", width = 5, height = 2, font = tkfont.Font(family = "Arial", size = 12, weight = tkfont.NORMAL), command = lambda : next(widgets, parameters, test_page))
-    b_no.place(relx = 0.5, x = 40, y = 240, anchor = "n")
+    browse = Button(stage, text = "Browse Directories", width = 20)
+    browse.place(relx = 0.5, x = 0, y = 60, anchor = "n")
 
-    widgets.append(b_no)
+    browse.config(command = lambda : sketch(stage, specifications, parameters, "Directory", dialog.askdirectory(initialdir = "/", title = "Select a Directory")))
 
-def directory_select(widgets, parameters):
-    dir = tkfiledialog.askdirectory(initialdir = "/", title = "Select a Directory")
+def start(stage, parameters):
+    start = Button(stage, text = "Start", width = 5)
+    start.place(relx = 0.5, x = 0, y = 320, anchor = "s")
+    
+    start.config(command = lambda : sketch(stage, directory, parameters, None, None))
 
-    parameters["Directory"] = dir
+def sketch(stage, scene, parameters, key, val):
+    if key and val:
+        parameters[key] = val
 
-    next(widgets, parameters, specifications)
+        print(parameters)
 
-def directory(parameters):
-    widgets = []
+    for widget in stage.winfo_children():
+        widget.destroy()
 
-    l_prompt = tk.Label(root, text = "Please choose the directory of analysis", font = tkfont.Font(family = "Arial", size = 12, weight = tkfont.NORMAL))
-    l_prompt.place(relx = 0.5, x = 0, y = 200, anchor = "n")
-
-    widgets.append(l_prompt)
-
-    b_browse = tk.Button(root, text = "Browse Directories", width = 20, height = 2, font = tkfont.Font(family = "Arial", size = 12, weight = tkfont.NORMAL), command = lambda : directory_select(widgets, parameters))
-    b_browse.place(relx = 0.5, x = 0, y = 240, anchor = "n")
-
-    widgets.append(b_browse)
-
-def start(parameters):
-    widgets = []
-
-    b_start = tk.Button(root, text = "Start", width = 5, height = 2, font = tkfont.Font(family = "Arial", size = 12, weight = tkfont.NORMAL), command = lambda : next(widgets, parameters, directory))
-    b_start.place(relx = 0.5, x = 0, y = 440, anchor = "n")
-
-    widgets.append(b_start)
+    scene(stage, parameters)
 
 if __name__ == "__main__":
     parameters = {
         "Directory" : None
     }
 
-    root = tk.Tk()
-    root_w = 800
-    root_h = 600
+    root = Tk()
 
     root.title("Phon Query to CSV")
-    root.geometry(str(root_w) + "x" + str(root_h))
+    root.geometry("800x600")
 
-    debug_frame = tk.Frame(root, width = root_w, height = root_h, highlightbackground = "red", highlightthickness = 4)
-    debug_frame.place(relx = 0.5, rely = 0, x = 0, y = 0, anchor = "n")
+    debug = Frame(root, width = 800, height = 600, highlightbackground = "red", highlightthickness = 1)
+    debug.place(relx = 0.5, rely = 0, x = 0, y = 0, anchor = "n")
 
-    l_title = tk.Label(root, text = "Phon Query to CSV", font = tkfont.Font(family = "Arial", size = 32, weight = tkfont.BOLD, underline = 1))
-    l_title.place(relx = 0.5, x = 0, y = 60, anchor = "n")
+    font.nametofont("TkDefaultFont").configure(size = 12)
 
-    l_subtitle = tk.Label(root, text = "A Visual Interface Assistance", font = tkfont.Font(family = "Arial", size = 20, weight = tkfont.BOLD))
-    l_subtitle.place(relx = 0.5, x = 0, y = 120, anchor = "n")
+    title = Label(root, text = "Phon Query to CSV")
+    title.place(relx = 0.5, x = 0, y = 60, anchor = "n")
 
-    b_quit = tk.Button(root, text = "Quit", width = 5, height = 2, font = tkfont.Font(family = "Arial", size = 12, weight = tkfont.NORMAL), command = lambda : root.destroy())
-    b_quit.place(relx = 0.5, x = 0, y = 500, anchor = "n")
+    title.configure(font = font.Font(size = 32, weight = font.BOLD, underline = 1))
 
-    start(parameters)
+    subtitle = Label(root, text = "A Visual Interface Assistance")
+    subtitle.place(relx = 0.5, x = 0, y = 120, anchor = "n")
 
-    root.mainloop()  # Keeps the window open   
+    subtitle.configure(font = font.Font(size = 20, weight = font.BOLD))
+
+    quit = Button(root, text = "Quit", width = 5)
+    quit.place(relx = 0.5, x = 0, y = 500, anchor = "n")
+
+    quit.configure(command = lambda : root.destroy())
+
+    stage = Frame(root, width = 650, height = 320, highlightbackground = "red", highlightthickness = 1)
+    stage.place(relx = 0.5, rely = 0, x = 0, y = 330, anchor = "center")
+
+    sketch(stage, start, parameters, None, None)
+
+    root.mainloop()  # Keeps the window open
 
 """
 
